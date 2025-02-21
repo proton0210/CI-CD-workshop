9f1148680b417443c7ed5883de57f17027548d13
Vidit Shah <vidit0210@protonmail.com>
2025-02-21 13:54:36 +0530

    DevStack

--- tests/constants.ts
+++ tests/constants.ts
@@ -1,4 +1,4 @@
-export const DEV_USER_POOL_ID = "ENTER_YOUR_DEV_USERPOOL_ID";
-export const DEV_USER_POOL_CLIENT_ID = "ENTER_YOUR_DEV_USERPOOL_CLIENT_ID";
-export const DEV_DYNAMO_TABLE_NAME = "ENTER_DEV_TABLE_NAME";
-export const DEV_REGION = "ENTER_DEV_REGION";
+export const DEV_USER_POOL_ID = "ap-south-1_ryeLU3nCi";
+export const DEV_USER_POOL_CLIENT_ID = "3ie37v951t2koprumc5da0m36v";
+export const DEV_DYNAMO_TABLE_NAME = "dev-table-CI-CD-Workshop";
+export const DEV_REGION = "ap-south-1";
