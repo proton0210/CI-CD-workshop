15f98f96af03430903232b80c8a87efbc25c96ac
Vidit Shah <vidit0210@protonmail.com>
2025-02-21 19:56:15 +0530

    Kicking off workflow

--- tests/constants.ts
+++ tests/constants.ts
@@ -1,4 +1,4 @@
-export const DEV_USER_POOL_ID = "ap-south-1_ryeLU3nCi";
-export const DEV_USER_POOL_CLIENT_ID = "3ie37v951t2koprumc5da0m36v";
-export const DEV_DYNAMO_TABLE_NAME = "dev-table-CI-CD-Workshop";
+export const DEV_USER_POOL_ID = "ap-south-1_Nd5XV0O7N";
+export const DEV_USER_POOL_CLIENT_ID = "7tqcu9kgf42kqjb3ifpujg90e7";
+export const DEV_DYNAMO_TABLE_NAME = "dev-post-confirmation-CI-CD-Workshop";
 export const DEV_REGION = "ap-south-1";
